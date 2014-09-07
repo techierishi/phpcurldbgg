f424b2cdf30481ddf63b795eb9b0140cecef2490
techierishi <techierishi@gmail.com>
2014-09-07 07:04:08 +0530

    Update dbg.php

--- dbg.php
+++ dbg.php
@@ -1,1 +1,12 @@
 <?php
+
+if(isset($_POST['message'])){
+	
+	$fo = fopen($_POST['filename'],$_POST['mode']);
+	
+	fwrite($fo,'[================[ '. date('d/m/Y H:m:s').' { '.$_POST['tag']. ' } ]================]');
+	fwrite($fo,$_POST['message']);
+
+	fclose($fo);
+}
+
