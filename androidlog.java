51b8d6a7b279ad4b6818d33abbf409502bfde172
Rishikesh <techierishi@gmail.com>
2016-02-26 00:04:26 +0530

    Update androidlog.java

--- androidlog.java
+++ androidlog.java
@@ -1,29 +1,51 @@
-public void appendLog(String text)
-	{       
-	   File logFile = new File("sdcard/cameralog.txt");
-	   if (!logFile.exists())
-	   {
-	      try
-	      {
-	         logFile.createNewFile();
-	      } 
-	      catch (IOException e)
-	      {
-	         // TODO Auto-generated catch block
-	         e.printStackTrace();
-	      }
-	   }
-	   try
-	   {
-	      //BufferedWriter for performance, true to set append to file flag
-	      BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true)); 
-	      buf.append(text);
-	      buf.newLine();
-	      buf.close();
-	   }
-	   catch (IOException e)
-	   {
-	      // TODO Auto-generated catch block
-	      e.printStackTrace();
-	   }
-	}
+  public enum COLOR {
+
+        RED,
+        GREEN,
+        BLUE
+    }
+
+    public synchronized static void appendLog(ServletContext context, String text, boolean doAppend, COLOR color) {
+
+        try {
+            String path = context.getRealPath("/debug.html");
+            File logFile = new File("" + path);
+
+            String ecolor = "";
+            if (color == COLOR.BLUE) {
+                ecolor = "#2073F0";
+            } else if (color == COLOR.GREEN) {
+                ecolor = "#009A57";
+            } else {
+                ecolor = "#D33E2A";
+            }
+
+            if (!logFile.exists()) {
+                try {
+                    logFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                // BufferedWriter for performance, true to set append to file flag
+                BufferedWriter buf = new BufferedWriter(new FileWriter(logFile,
+                        doAppend));
+
+                Calendar cal = Calendar.getInstance();
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                String v_time = sdf.format(cal.getTime());
+
+                buf.append(" <p style=\"color:" + ecolor + ";\">[" + v_time + "] " + text
+                        + "</p>");
+
+                buf.newLine();
+                buf.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
